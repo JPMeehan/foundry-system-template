c87f1421e8cf1a9748d3586356082ec41421944d
Joseph <JPMeehan5@Gmail.com>
2025-05-06 08:42:38 -0700

    Basic sheet API

--- module/apps/sheets/actor-sheet.mjs
+++ module/apps/sheets/actor-sheet.mjs
@@ -1,3 +1,48 @@
 const {api, sheets} = foundry.applications;
 
-export default class SystemActorSheet extends api.HandlebarsApplicationMixin(sheets.ActorSheetV2) {}
+export default class SystemActorSheet extends api.HandlebarsApplicationMixin(sheets.ActorSheetV2) {
+  /** @inheritdoc */
+  static DEFAULT_OPTIONS = {
+    classes: ["actor", "foundry-system-template"],
+    form: {
+      submitOnChange: true
+    },
+    window: {
+      resizable: true
+    }
+  };
+
+  /** @inheritdoc */
+  static TABS = {};
+
+  /** @inheritdoc */
+  static PARTS = {};
+
+  /* -------------------------------------------- */
+  /*  Rendering                                   */
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+
+    return context;
+  }
+
+  /* -------------------------------------------- */
+  /*  Public API                                  */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Action Event Handlers                       */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Drag and Drop                               */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Compatibility and Deprecations              */
+  /* -------------------------------------------- */
+
+}
--- module/apps/sheets/item-sheet.mjs
+++ module/apps/sheets/item-sheet.mjs
@@ -1,3 +1,48 @@
 const {api, sheets} = foundry.applications;
 
-export default class SystemItemSheet extends api.HandlebarsApplicationMixin(sheets.ItemSheetV2) {}
+export default class SystemItemSheet extends api.HandlebarsApplicationMixin(sheets.ItemSheetV2) {
+  /** @inheritdoc */
+  static DEFAULT_OPTIONS = {
+    classes: ["item", "foundry-system-template"],
+    form: {
+      submitOnChange: true
+    },
+    window: {
+      resizable: true
+    }
+  };
+
+  /** @inheritdoc */
+  static TABS = {};
+
+  /** @inheritdoc */
+  static PARTS = {};
+
+  /* -------------------------------------------- */
+  /*  Rendering                                   */
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+
+    return context;
+  }
+
+  /* -------------------------------------------- */
+  /*  Public API                                  */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Action Event Handlers                       */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Drag and Drop                               */
+  /* -------------------------------------------- */
+
+  /* -------------------------------------------- */
+  /*  Compatibility and Deprecations              */
+  /* -------------------------------------------- */
+
+}
