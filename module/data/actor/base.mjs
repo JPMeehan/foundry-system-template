4e6b7b32c9c6048584774c8053ae3b8cda4297ec
Joseph <JPMeehan5@Gmail.com>
2025-06-02 11:44:13 -0700

    Dependency updates

--- module/data/actor/base.mjs
+++ module/data/actor/base.mjs
@@ -2,6 +2,21 @@
  * A shared implementation for the system data model for actors
  */
 export default class BaseActorModel extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+
+    return {
+      hp: new fields.SchemaField({
+        value: new fields.NumberField({integer: true, min: 0}),
+        max: new fields.NumberField({integer: true, min: 0})
+      }),
+      biography: new fields.SchemaField({
+        description: new fields.HTMLField(),
+        gm: new fields.HTMLField()
+      })
+    };
+  }
+
   /**
    * Perform item subtype specific modifications to the actor roll data
    * @param {object} rollData   Pointer to the roll data object
--- module/data/actor/character.mjs
+++ module/data/actor/character.mjs
@@ -3,6 +3,8 @@ import BaseActorModel from "./base.mjs";
 export default class CharacterData extends BaseActorModel {
   /** @inheritdoc */
   static defineSchema() {
-    return {};
+    const schema = super.defineSchema();
+
+    return schema;
   }
 }
