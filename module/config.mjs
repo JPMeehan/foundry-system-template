324f814f278d91e45b414f10bb6c1d60c649f2ce
Joseph <JPMeehan5@Gmail.com>
2025-06-03 14:06:14 -0700

    Item sheet fields

--- module/config.mjs
+++ module/config.mjs
@@ -7,3 +7,29 @@
  * @type {Record<string, StatusEffectConfig>}
  */
 export const conditions = {};
+
+/**
+ * @typedef ItemProperty
+ * @property {string} label           The i18n string for the
+ * @property {Set<string>} itemTypes  Item types that can have this property
+ */
+
+/**
+ * A simple example of a configurable list of item properties
+ * Modules can adjust this
+ * @type {Record<string, ItemProperty>}
+ */
+export const itemProperties = {
+  supernatural: {
+    label: "FoundrySystemTemplate.Item.Properties.supernatural",
+    itemTypes: new Set(["equipment", "feature"]),
+  },
+  metal: {
+    label: "FoundrySystemTemplate.Item.Properties.metal",
+    itemTypes: new Set(["equipment"]),
+  },
+  epic: {
+    label: "FoundrySystemTemplate.Item.Properties.epic",
+    itemTypes: new Set(["feature"]),
+  },
+};
